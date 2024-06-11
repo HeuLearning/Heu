7f8becd0d559206e2477d25539e707cec0ff17d7
Julia Ying <juliaying@princeton.edu>
2024-06-11 13:37:34 -0400

    updated front end

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,9 +2,6 @@ import { type AppType } from "next/dist/shared/lib/utils";
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 import "~/styles/globals.css";
 
-
-
-
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <UserProvider>
