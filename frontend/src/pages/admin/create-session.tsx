6a3223578511d8d51266ac153114a9a988eda1df
Julia Ying <juliaying@princeton.edu>
2024-06-25 09:50:33 -0400

    admin page 3 buttons + sessions page can display sessions and delete

--- frontend/src/pages/admin/create-session.tsx
+++ frontend/src/pages/admin/create-session.tsx
@@ -1,11 +1,13 @@
 import Head from "next/head";
-import { useUser } from '@auth0/nextjs-auth0/client';
+import { useUser } from "@auth0/nextjs-auth0/client";
 import { useEffect } from "react";
-// import { useRouter } from 'next/router';
-import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
-import { withPageAuthRequired, getSession } from '@auth0/nextjs-auth0';
-import { getAccessToken } from '@auth0/nextjs-auth0';
-import { redirect } from 'next/navigation'
+import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
+import { withPageAuthRequired, getSession } from "@auth0/nextjs-auth0";
+import { getAccessToken } from "@auth0/nextjs-auth0";
+import { redirect } from "next/navigation";
+import { useRouter } from "next/router";
+import { Session } from "../../../models/session";
+import { string } from "zod";
 
 export const getServerSideProps = withPageAuthRequired({
   async getServerSideProps(ctx) {
@@ -15,34 +17,37 @@ export const getServerSideProps = withPageAuthRequired({
     if (!session) {
       return {
         redirect: {
-          destination: '/api/auth/login',
+          destination: "/api/auth/login",
           permanent: false,
         },
       };
     }
 
     const options = {
-      method: 'GET',
+      method: "GET",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${session.accessToken}`, // Include the access token
+        Authorization: `Bearer ${session.accessToken}`, // Include the access token
       },
     };
 
-    const response = await fetch('http://localhost:8000/api/get-user-role', options);
+    const response = await fetch(
+      "http://localhost:8000/api/get-user-role",
+      options
+    );
     const roleType = await response.json();
     const role = roleType.role;
     if (role === "in") {
       return {
         redirect: {
-          destination: '/instructor',
+          destination: "/instructor",
           permanent: false,
         },
       };
     } else if (role === "st") {
       return {
         redirect: {
-          destination: '/learner',
+          destination: "/learner",
           permanent: false,
         },
       };
@@ -53,32 +58,92 @@ export const getServerSideProps = withPageAuthRequired({
       return {
         props: {
           role: roleType || null,
-          sessions: null,
+          organization: "",
+          sessions: [],
+          sessionToken: session.accessToken || null,
         },
       };
     }
 
     // if the user is verified then get the related sessions
     const sessionOptions = {
-      method: 'GET',
+      method: "GET",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${session.accessToken}`, // Include the access token
+        Authorization: `Bearer ${session.accessToken}`, // Include the access token
       },
     };
 
-    let sessionResponse = await fetch('http://localhost:8000/api/sessions', sessionOptions);
-    sessionResponse = await sessionResponse.json();
+    let sessionResponse = await fetch(
+      "http://localhost:8000/api/admin-sessions",
+      sessionOptions
+    );
+    const sessionData: { learning_organization: string; sessions: Session[] } =
+      await sessionResponse.json();
     console.log(sessionResponse);
     return {
-        props: {
-          role: roleType || null,
-          sessions: sessionResponse || null,
-        },
-      };
-  }
+      props: {
+        role: roleType || null,
+        organization: sessionData.learning_organization || "",
+        sessions: sessionData.sessions || [],
+        sessionToken: session.accessToken || null,
+      },
+    };
+  },
 });
 
-export default function CreateSessions() {
-    
+export default function Sessions({
+  role,
+  organization,
+  sessions,
+  sessionToken,
+}) {
+  console.log(sessions);
+  const router = useRouter();
+
+  const refreshData = () => {
+    router.replace(router.asPath);
+  };
+
+  async function handleDelete(sessionId) {
+    const res = await fetch(
+      `http://localhost:8000/api/admin-session-detail/${sessionId}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${sessionToken}`, // Include the access token
+        },
+      }
+    );
+
+    if (res.status < 300) {
+      refreshData();
+    }
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Heu Learning</title>
+        <meta name="description" content="Teach more English better" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/icon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <div>
+        <form>
+          <label htmlFor="fname">Start time:</label>
+          <input type="text" id="fname" name="fname"></input>
+          <label htmlFor="lname">End time:</label>
+          <input type="text" id="lname" name="lname"></input>
+
+          <input type="submit" value="Submit"></input>
+        </form>
+      </div>
+    </>
+  );
 }
