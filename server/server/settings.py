95914e266ddd275230705d226ca1257e0adc3e7e
Desi DeVaul <desi.devaul@gmail.com>
2024-07-01 14:50:14 -0400

    changed instructor-applications-instructor/<int:template_id> route to also return the link to the google form. also begn implementing logic for email server

--- server/server/settings.py
+++ server/server/settings.py
@@ -225,10 +225,10 @@ AUTH_USER_MODEL = 'heu.CustomUser'
 # STATIC_URL = '/static/'
 # django_heroku.settings(locals())
 
-# SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
-# EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
