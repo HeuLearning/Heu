7b1b722018c14e820013d656cafb4a82ff64e606
jbao-12 <176948788+jbao-12@users.noreply.github.com>
2024-08-13 13:13:09 -0400

    Add box shadow.

--- frontend/components/instructor/mobile/ButtonBar.tsx
+++ frontend/components/instructor/mobile/ButtonBar.tsx
@@ -10,7 +10,7 @@ export default function ButtonBar({
   secondaryButtonText,
 }: ButtonBarProps) {
   return (
-    <div className="fixed bottom-0 z-50 flex h-[65px] w-full items-center justify-center border-t-[1px] border-surface_border_tertiary bg-white p-[8px]">
+    <div className="fixed bottom-0 z-50 flex h-[65px] w-full items-center justify-center border-t-[1px] border-surface_border_tertiary bg-white p-[8px] shadow-200">
       <div className="flex w-full  gap-[8px]">
         {secondaryButtonText && (
           <Button className="button-secondary h-full w-full rounded-[10px]">
