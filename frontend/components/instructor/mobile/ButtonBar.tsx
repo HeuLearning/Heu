6f0acaa487e07167a8d89cc731741c839c7dc98e
jbao-12 <176948788+jbao-12@users.noreply.github.com>
2024-08-13 13:05:59 -0400

    Append border- to color.

--- frontend/components/instructor/mobile/ButtonBar.tsx
+++ frontend/components/instructor/mobile/ButtonBar.tsx
@@ -10,7 +10,7 @@ export default function ButtonBar({
   secondaryButtonText,
 }: ButtonBarProps) {
   return (
-    <div className="surface_border_tertiary fixed bottom-0 z-50 flex h-[65px] w-full items-center justify-center border-t-[1px] bg-white p-[8px]">
+    <div className="fixed bottom-0 z-50 flex h-[65px] w-full items-center justify-center border-t-[1px] border-surface_border_tertiary bg-white p-[8px]">
       <div className="flex w-full  gap-[8px]">
         {secondaryButtonText && (
           <Button className="button-secondary h-full w-full rounded-[10px]">
