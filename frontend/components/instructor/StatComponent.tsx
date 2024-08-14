4fee5732339fab8f7a073abc574a6c50ba67f286
jbao-12 <176948788+jbao-12@users.noreply.github.com>
2024-08-14 14:27:43 -0400

    Implement mobile class detail view
    Class detail view would become misarranged after
    changing from desktop. Used specifications for
    mobile view to preserve functioning layout.
    
    [Ticket: X]

--- frontend/components/instructor/StatComponent.tsx
+++ frontend/components/instructor/StatComponent.tsx
@@ -11,24 +11,33 @@ export default function StatComponent({
     <div className="flex gap-[16px]">
       <div
         className={`relative  ${
-          isMobile ? "h-[24px] w-[24px]" : "h-[40px] w-[40px]"
+          isMobile ? "h-[44px] w-[44px]" : "h-[40px] w-[40px]"
         }`}
       >
         <div className="z-10 center-atop-svg">{children}</div>
         <svg
-          width={isMobile ? "24" : "40"}
-          height={isMobile ? "24" : "40"}
+          width={isMobile ? "44" : "40"}
+          height={isMobile ? "44" : "40"}
           viewBox="0 0 40 40"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <circle cx="20" cy="20" r="20" fill={`var(--${bgColor})`} />
+          <circle
+            cx="20"
+            cy="20"
+            r="20"
+            fill={
+              isMobile ? `var(--surface_border_tertiary)` : `var(--${bgColor})`
+            }
+          />
         </svg>
       </div>
       <div className="space-y-[3px]">
         <h2
           className={`text-typeface_primary ${
-            isMobile ? "text-body-semibold" : "text-h2"
+            isMobile
+              ? "h-[22px] font-[600] tracking-[-1%] text-[16px] leading-[22px]"
+              : "text-h2"
           }`}
         >
           {heading}
@@ -36,7 +45,7 @@ export default function StatComponent({
         <h2
           className={`text-typeface_secondary ${
             isMobile
-              ? "text-body-regular-cap-height"
+              ? "h-[26px] font-[400] tracking-[-1%] text-[16px] leading-[26px]"
               : "text-body-medium-cap-height"
           }`}
         >
