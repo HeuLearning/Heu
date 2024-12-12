41d858bb93274549a45a17ff0f49b73be0ee7c59
Jordan Perry-Greene <jordypg@gmail.com>
2024-12-12 11:11:06 -0500

    instructorcontent no longer requires onComplete

--- components/exercises/InstructorContent.tsx
+++ components/exercises/InstructorContent.tsx
@@ -3,19 +3,19 @@ import Markdown from 'react-markdown'; // Import react-markdown
 import remarkGfm from 'remark-gfm'
 
 interface InstructionProps {
-  instruction: string; // Define the instruction prop as a string
-  onComplete: () => void; // Define the onComplete prop
+    instruction: string; // Define the instruction prop as a string
+    //onComplete: () => void; // Define the onComplete prop
 }
 
 export default function Instruction({
-  instruction,
-  onComplete,
+    instruction,
+    //onComplete,
 }: InstructionProps) {
-  return (
-    <div>
-      <h2 className="text-typeface_primary text-h3">
-        <Markdown remarkPlugins={[remarkGfm]}>{instruction}</Markdown>
-      </h2>
-    </div>
-  );
+    return (
+        <div>
+            <h2 className="text-typeface_primary text-h3">
+                <Markdown remarkPlugins={[remarkGfm]}>{instruction}</Markdown>
+            </h2>
+        </div>
+    );
 }
